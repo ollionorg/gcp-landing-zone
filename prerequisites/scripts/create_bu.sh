025fdbea6ab957150def73c91760c08395513df5
vishal-kapse <vishal.kapse@cldcvr.com>
2023-03-04 10:13:06 +0530

    Update prerequisite bu creation script to include bu creation in 5-app-infra and update bucket names and readme file

--- prerequisites/scripts/create_bu.sh
+++ prerequisites/scripts/create_bu.sh
@@ -23,6 +23,7 @@ then
     BU_NAME="${1}"
     BU_CODE="${2}"
 
+    #4-projects
     NEW_BU_FOLDER="../../4-projects/${BU_NAME}"
 
     cp -r ../../4-projects/BU_NAME ${NEW_BU_FOLDER}
@@ -46,6 +47,27 @@ then
     -D BU_NAME_staging=${BU_NAME}_staging -D BU_NAME_production=${BU_NAME}_production \
     -D UPDATE_BACKEND_BUCKET=${BKT_NAME} {} > {}.m4 && cat {}.m4 > {} && rm {}.m4" \;
 
+    #5-app-infra
+    NEW_BU_FOLDER_APP="../../5-app-infra/${BU_NAME}"
+
+    cp -r ../../5-app-infra/BU_NAME ${NEW_BU_FOLDER_APP}
+
+    echo -e "bu_name = \"${BU_NAME}\"\nbu_code = \"${BU_CODE}\"" > ${NEW_BU_FOLDER_APP}/development/bu.auto.tfvars
+    echo -e "bu_name = \"${BU_NAME}\"\nbu_code = \"${BU_CODE}\"" > ${NEW_BU_FOLDER_APP}/staging/bu.auto.tfvars
+    echo -e "bu_name = \"${BU_NAME}\"\nbu_code = \"${BU_CODE}\"" > ${NEW_BU_FOLDER_APP}/production/bu.auto.tfvars
+
+    find ${NEW_BU_FOLDER_APP} -type f -iname "*.md"\
+    -exec bash -c "m4 -D BU_NAME=${BU_NAME} -D BU_NAME_shared=${BU_NAME}_shared \
+    -D BU_NAME_development=${BU_NAME}_development \
+    -D BU_NAME_shared=${BU_NAME}_shared -D BU_NAME_staging=${BU_NAME}_staging \
+    -D BU_NAME_production=${BU_NAME}_production {} > {}.m4  && cat {}.m4 > {} && rm {}.m4" \;
+
+    find ${NEW_BU_FOLDER_APP} -type f \( -name 'backend.tf' -o -name 'remote-state.tf' -o -name 'locals.tf' \) \
+    -prune ! -type l -print -print  -exec bash -c "m4 \
+    -D BU_NAME=${BU_NAME} -D BU_NAME_shared=${BU_NAME}_shared \
+    -D BU_NAME_development=${BU_NAME}_development \
+    -D BU_NAME_staging=${BU_NAME}_staging -D BU_NAME_production=${BU_NAME}_production \
+    -D UPDATE_BACKEND_BUCKET=${BKT_NAME} {} > {}.m4 && cat {}.m4 > {} && rm {}.m4" \;
     find ../../.github/workflows -type f -iname "*BU_NAME*.yml.example" -exec bash -c ' cp $0 ${0/BU_NAME/'$BU_NAME'}' {} \;
 
     for f in ../../.github/workflows/*${BU_NAME}*
